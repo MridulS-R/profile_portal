653d2c10ea1e17b02a9fc988a0f29bf22058b84f
MridulS-R <sr.mridulshukla9@gmail.com>
2025-09-14 05:23:48 +0530

    addign

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,4 +14,9 @@ Rails.application.configure do
   config.i18n.fallbacks = true
   config.active_support.report_deprecations = false
   config.lograge.enabled = true
+  # If your app is accessed via multiple hostnames or proxies, origin checks can
+  # cause CSRF 422s when the Origin header differs. Disable with caution.
+  if ENV["DISABLE_FORGERY_ORIGIN_CHECK"] == "true"
+    config.action_controller.forgery_protection_origin_check = false
+  end
 end
